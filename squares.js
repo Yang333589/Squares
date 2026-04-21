64f6bf390f87ac73533d0af07785f02d6340c73d
Monica Yang <1212monicayang@gmail.com>
2026-04-20 21:11:45 -0400

    Made Add Square Button

--- squares.js
+++ squares.js
@@ -1,23 +1,42 @@
 let squareArea = document.querySelector("#squarearea");
-let squareCount = parseInt(Math.random()*21)+30; //30-50 squares
+let squareCount = parseInt(Math.random() * 21) + 30; //30-50 squares
+console.log(squareCount);
 
-for (let i = 0; i < squareCount; i++){
+for (let i = 0; i < squareCount; i++) {
+    //make a square
+    addSquares();
+}
+
+function addSquares() {
     //make a square
     let square = document.createElement("div");
     square.className = "square";
-    square.style.left = parseInt(Math.random()*650) + "px";
-    square.style.top = parseInt(Math.random()*250) + "px";
+    square.style.left = parseInt(Math.random() * 650) + "px";
+    square.style.top = parseInt(Math.random() * 250) + "px";
     square.style.backgroundColor = getRandomColor();
-    //put it on screen
     squareArea.append(square);
 }
 
-function getRandomColor(){
+
+function getRandomColor() {
     //make a color
     let letters = "0123456789ABCDEF";
     let color = "#";
-    for (let i = 0; i < 6; i++){
-        color += letters.charAt(parseInt(Math.random()*letters.length));
+    for (let i = 0; i < 6; i++) {
+        color += letters.charAt(parseInt(Math.random() * letters.length));
     }
     return color;
 }
+
+function changeColor() {
+    //an array
+    let allSquares = document.querySelectorAll(".square");
+    //for loop
+}
+
+
+//make a function that add one square (make sure that it is the onclick for the button)
+//make a funtion that changes all the colors of all the squares (make sure the color button calls it)
+//clicking on the square makes it move on top (z-index = 100)
+//clicking on teh square again deletes the square
+//randomize the square size 
